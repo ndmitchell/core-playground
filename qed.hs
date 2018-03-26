c0f0ac3fa45de45e20a3244a778b4de1aadf6e7c
Neil Mitchell <ndmitchell@gmail.com>
2018-03-26 20:49:23 +0100

    Uncomment a few more examples

--- qed.hs
+++ qed.hs
@@ -14,6 +14,7 @@ proof3 f' = (($) . f') === eta2 f'
 
 proof4 n x = take n (repeat x) === replicate n x
 
+-- These aren't the same unless n is positive, as n=(-1) x=[1] is 1 vs _|_
 -- proof5 n x = head (drop n x) === x !! n
 
 proof6 f z g x = foldr f z (map g x) === foldr (f . g) z x
@@ -24,20 +25,22 @@ proof8 f x y = zipWith f (repeat x') === map (f x')
 
 proof9 x y = (if x then False else y) === not x && y
 
--- proof10 = map fromJust . filter isJust === catMaybes
+proof10 = map fromJust . filter isJust === catMaybes
 
--- proof11 = mapMaybe id === catMaybes
+proof11 = mapMaybe id === catMaybes
 
 proof12 f x = map f (repeat x) === repeat (f x)
 
 proof13 f x y = map (uncurry f) (zip x y) === zipWith f x y
 
+-- Results in non-termination trying to prove iterate (id*) = repeat, for increasingly more id's
 -- proof14 = iterate id === repeat
 
--- proof15 f x = catMaybes (map f x) === mapMaybe f x
+proof15 f x = catMaybes (map f x) === mapMaybe f x
 
--- proof16 = concatMap maybeToList === catMaybes
+proof16 = concatMap maybeToList === catMaybes
 
--- proof17 f g x = concatMap f (map g x) === concatMap (f . g) x
+proof17 f g x = concatMap f (map g x) === concatMap (f . g) x
 
+-- Results in non-termination because you are building up progressively bigger (:) accumulator
 -- proof18 x = head (reverse x) === last x
